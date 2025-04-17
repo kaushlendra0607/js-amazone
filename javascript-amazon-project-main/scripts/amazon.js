d691445ca209fbc2ac38edb630a5616b7d19d15e
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-04-17 23:17:24 +0530

    to make cart qty. interactive

--- javascript-amazon-project-main/scripts/amazon.js
+++ javascript-amazon-project-main/scripts/amazon.js
@@ -113,9 +113,11 @@ document.querySelectorAll('.js-addto-cart').forEach((button)=>{
         quantity:1
        })
     }
-
-   
-   console.log(cart);
-   
+    let cartquantity = 0;
+    cart.forEach((item)=>{
+      cartquantity+=item.quantity;
+    })
+    document.querySelector('.js-cart-quantity').innerHTML=cartquantity;
+  
   })
 })
