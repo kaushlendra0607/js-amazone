8172864ed1daa084518bce612b6b6a7f5752a7af
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-04-19 11:24:48 +0530

    save cart to localstorage

--- javascript-amazon-project-main/data/cart.js
+++ javascript-amazon-project-main/data/cart.js
@@ -1,4 +1,6 @@
-export let cart =[{
+export let cart =JSON.parse(localStorage.getItem('cart'));
+if(!cart){
+  cart=[{
     productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity:2
 },
@@ -7,7 +9,10 @@ export let cart =[{
     quantity:1
 }
 ];
-
+}
+function saveToStorage(){
+  localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 export function addtoCart(productId){
   let matchingitem;
@@ -24,6 +29,7 @@ export function addtoCart(productId){
         quantity:1
        })
     }
+    saveToStorage()
 }
 
 export function removeFromCart(productId){
@@ -34,4 +40,5 @@ export function removeFromCart(productId){
     }
   })
   cart =newCart;
+  saveToStorage()
 }
