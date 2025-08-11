62dfd6219a811e776c4db38caab3ce7945324c0e
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-11 12:04:21 +0530

    fixed an eslint error in App.jsx and added some comments

--- react-course/chatbot-project/src/App.jsx
+++ react-course/chatbot-project/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react'
+import { useState } from 'react'
 import { ChatInput } from './components/ChatInput'
 import ChatMessages from './components/ChatMessages';
 
