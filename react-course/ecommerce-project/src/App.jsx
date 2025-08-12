7a33d639d6f26802fd81649c100db83d2b13996b
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-12 10:21:18 +0530

    move checkout page into react

--- react-course/ecommerce-project/src/App.jsx
+++ react-course/ecommerce-project/src/App.jsx
@@ -1,4 +1,5 @@
 import { HomePage } from './pages/HomePage'
+import { CheckoutPage } from './pages/CheckoutPage'
 import {Routes,Route} from 'react-router'//for routing we need to put our App in Routes and Route Routes tells react that there are several pages and Route ia basically a page
 import './App.css'
 
@@ -9,7 +10,7 @@ function App() {
       <Route index /* path tells the url path of the page only a slash means url is empty which will take us to home page and index does the same thing as path="/"*/ 
       element ={ <HomePage/>}//element tells react which element to display
       />
-      <Route path="checkout" element={<div><p>Hellooo</p></div>}/>{/* this way the html of index file is shared and now we'll juast have to create the elements and put them here it's called SPA single page application*/}
+      <Route path="checkout" element={<CheckoutPage/>}/>{/* this way the html of index file is shared and now we'll juast have to create the elements and put them here it's called SPA single page application*/}
     </Routes>
   )
 }
