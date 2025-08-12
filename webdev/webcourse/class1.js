2056d0cf2ba3d3fa6246b41d15bf72055ed0b9ac
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-12 08:52:11 +0530

    nothing much

--- webdev/webcourse/class1.js
+++
@@ -1,7 +0,0 @@
-function greet(){
-    console . log("hello world !")
-}
-function greetaliens(){
-    console.log("hello aliens !")
-}
-setTimeout(greetaliens ,3*1000)//setTimeout is built in finction. Chat gpt it. 
