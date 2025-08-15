43fa97b8069a9e88eec5e4be6abb2f7bc4fb559e
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-15 10:02:49 +0530

    added NavLink

--- react-course/ecommerce-project/src/Components/Header.jsx
+++ react-course/ecommerce-project/src/Components/Header.jsx
@@ -1,17 +1,17 @@
-import { Link } from 'react-router';//the <a> tag of html reloads by default but when we're creating ab SPA we dont need to reload For this React gives us Link Element
+import { Link, NavLink } from 'react-router';//the <a> tag of html reloads by default but when we're creating ab SPA we dont need to reload For this React gives us Link Element
 import './header.css'//Link element lets us go to another page without reloading
-
+//NavLink is used generally for navigation links like the links in header of a page The additional Feature it has is that it knows which website is being loaded unlike Link It adds a specific class to the active link for more use gpt
 export function Header() {//Link doesnt uses href it uses to
     return (
         <>
             <div className="header">
                 <div className="left-section">
-                    <Link to="/" className="header-link">
+                    <NavLink to="/" className="header-link">
                         <img className="logo"
                             src="images/logo-white.png" />
                         <img className="mobile-logo"
                             src="images/mobile-logo-white.png" />
-                    </Link>
+                    </NavLink>
                 </div>
 
                 <div className="middle-section">
@@ -23,16 +23,16 @@ export function Header() {//Link doesnt uses href it uses to
                 </div>
 
                 <div className="right-section">
-                    <Link className="orders-link header-link" to="/orders">
+                    <NavLink className="orders-link header-link" to="/orders">
 
                         <span className="orders-text">Orders</span>
-                    </Link>
+                    </NavLink>
 
-                    <Link className="cart-link header-link" to="/checkout">
+                    <NavLink className="cart-link header-link" to="/checkout">
                         <img className="cart-icon" src="images/icons/cart-icon.png" />
                         <div className="cart-quantity">3</div>
                         <div className="cart-text">Cart</div>
-                    </Link>
+                    </NavLink>
                 </div>
             </div>
         </>
