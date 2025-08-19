9105b695aa36050da736e75ef1eeb238c99de491
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-19 23:29:51 +0530

    created orders grid component for orders page

--- react-course/ecommerce-project/src/main.jsx
+++ react-course/ecommerce-project/src/main.jsx
@@ -5,7 +5,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <StrictMode>{/* strictmode is given by react for idempotency means catching bugs or errors by running web twice gpt for more */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
