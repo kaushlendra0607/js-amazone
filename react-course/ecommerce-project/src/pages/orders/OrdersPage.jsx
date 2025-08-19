e4bc579dcc802e011534c6d0159f07197a6e506e
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-19 22:46:44 +0530

    used async await in orders page and created another component of cartItems for order summary

--- react-course/ecommerce-project/src/pages/orders/OrdersPage.jsx
+++ react-course/ecommerce-project/src/pages/orders/OrdersPage.jsx
@@ -11,11 +11,16 @@ export function OrdersPage({cart}){
     const [orders, setOrders] = useState([]);
 
     useEffect(()=>{
-        axios.get('/api/orders?expand=products')
-            .then((response)=>{
-                setOrders(response.data)
-                console.log(response.data);
-            })
+        const fetchOrdersData = async ()=>{
+            const response = await axios.get('/api/orders?expand=products');
+            setOrders(response.data)
+        }
+        fetchOrdersData();
+        // axios.get('/api/orders?expand=products')
+        //     .then((response)=>{
+        //         setOrders(response.data)
+        //         console.log(response.data);
+        //     })
     },[])
 
     return(
