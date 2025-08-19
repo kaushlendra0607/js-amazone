6f57e5b80f3379c4f9a61db0d7ce9e8747eba9cd
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-19 23:00:57 +0530

    created separate component of delivery date for order summary in checkout

--- react-course/ecommerce-project/src/pages/checkout/OrderSummary.jsx
+++ react-course/ecommerce-project/src/pages/checkout/OrderSummary.jsx
@@ -1,20 +1,17 @@
-import dayjs from "dayjs";
+// import dayjs from "dayjs";
 // import { formatMoney } from "../../utils/money";
 import { DeliveryOptions } from "./Deliveryoptions";
 import { CartItemDetails } from "./cartItemDetails";
+import { DeliveryDate } from "./DeliveryDate";
 
 export function OrderSummary({ cart, deliveryOptions }){
     return(
             <div className="order-summary">
             {deliveryOptions.length>0 && cart.map((cartItem)=>{
-                const selectedDeliveryOption =deliveryOptions.find((deliveryOption)=>{
-                    return deliveryOption.id === cartItem.deliveryOptionId;
-                })
+
                 return(
             <div key={cartItem.productId} className="cart-item-container">
-                <div className="delivery-date">
-                Delivery date: {dayjs(selectedDeliveryOption.estimatedDeliveryTimeMs).format('dddd, MMMM D')}
-                </div>
+                <DeliveryDate cartItem={cartItem} deliveryOptions={deliveryOptions}/>
 
                 <div className="cart-item-details-grid">
                 
