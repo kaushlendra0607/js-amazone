219e354ea045f16bdedb9debd1c2d23290f3fdc5
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-08-30 03:07:39 +0530

    Made update button to work on checkoutPage

--- react-course/ecommerce-project/src/pages/checkout/cartItemDetails.jsx
+++ react-course/ecommerce-project/src/pages/checkout/cartItemDetails.jsx
@@ -1,9 +1,14 @@
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import { formatMoney } from "../../utils/money";
 import axios from "axios";
 
 
 export function CartItemDetails({ cartItem, loadCart }) {
+    const [quantity,setQuantity] = useState(cartItem.quantity);
+    const [isTracked,setIsTracked] = useState(false);
+    function inputQuantity(e){
+        setQuantity(e.target.value);
+    }
     const deleteCartItem = async()=>{
         await axios.delete(`/api/cart-items/${cartItem.productId}`);
         await loadCart();
@@ -19,10 +24,23 @@ export function CartItemDetails({ cartItem, loadCart }) {
                     {formatMoney(cartItem.product.priceCents)}
                 </div>
                 <div className="product-quantity">
+                    <input type="text" onChange={inputQuantity} value={quantity} style={{width:'50px',display:isTracked?"inline-block":"none"}}/>
                     <span>
                         Quantity: <span className="quantity-label">{cartItem.quantity}</span>
                     </span>
-                    <span className="update-quantity-link link-primary">
+                    <span className="update-quantity-link link-primary"
+                        onClick={async ()=>{
+                            if(isTracked){
+                                await axios.put(`/api/cart-items/${cartItem.productId}`, {
+                                quantity: Number(quantity),
+                            });
+                            await loadCart();
+                            setIsTracked(false);
+                            }else{
+                                 setIsTracked(true);
+                            }
+                        }}
+                    >
                         Update
                     </span>
                     <span className="delete-quantity-link link-primary"
