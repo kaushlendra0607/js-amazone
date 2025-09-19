07b7640426a8122b2824734b4a343e1bfeb40871
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-09-19 19:52:17 +0530

    Renamed money from js to ts and added a variable type.

--- react-course/ecommerce-project-ts/src/utils/money.ts
+++ react-course/ecommerce-project-ts/src/utils/money.ts
@@ -1,4 +1,4 @@
 
-export function formatMoney(amountCents){
+export function formatMoney(amountCents: number){
     return `${(amountCents * 0.01 ).toFixed(2)}`//or just devide by 100 would also work and is actually better than * 0.01
 }
