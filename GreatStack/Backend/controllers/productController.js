4350b74b1b156a952fa3d5d0390eb082f364b40e
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-11-29 20:44:07 +0530

    Completed the product router controllers.

--- GreatStack/Backend/controllers/productController.js
+++ GreatStack/Backend/controllers/productController.js
@@ -9,12 +9,12 @@ const addProduct = async (req, res) => {
         const image2 = req.files.image2 && req.files.image2[0];
         const image3 = req.files.image3 && req.files.image3[0];
         const image4 = req.files.image4 && req.files.image4[0];
-        const images = [image1,image2,image3,image4].filter((item)=>item != undefined);
+        const images = [image1, image2, image3, image4].filter((item) => item != undefined);
         /*Without Promise.all, images would upload one by one (slow).
         With Promise.all, all images upload at once (fast). */
         const imagesUrl = await Promise.all(
-            images.map(async(img)=>{
-                let result = await v2.uploader.upload(img.path,{resource_type:'image'});
+            images.map(async (img) => {
+                let result = await v2.uploader.upload(img.path, { resource_type: 'image' });
                 console.log(result.resource_type);
                 console.log(`\n${result.format}\n`);
                 console.log(result.secure_url);
@@ -25,13 +25,13 @@ const addProduct = async (req, res) => {
         const productData = {
             name,
             description,
-            price : Number(price),
-            bestseller : bestseller == "true" ? true:false,
+            price: Number(price),
+            bestseller: bestseller == "true" ? true : false,
             category,
             subCategory,
             sizes,
-            image : imagesUrl,
-            date : Date.now()
+            image: imagesUrl,
+            date: Date.now()
         }
 
         const product = new productModel(productData);
@@ -39,7 +39,7 @@ const addProduct = async (req, res) => {
 
         console.log(images);
         console.log(imagesUrl);
-        res.json({success:true,message:"product saved"});
+        res.json({ success: true, message: "product saved" });
 
     } catch (error) {
         console.log("Adding product Error.");
@@ -49,13 +49,38 @@ const addProduct = async (req, res) => {
 
 const listProduct = async (req, res) => {
 
+    try {
+        const products = await productModel.find({});
+        res.json({ success: true, products });
+    } catch (error) {
+        console.log("Couldn't load products", error);
+        res.json({ success: false, message: error.message });
+    }
+
 }
 
 const removeProduct = async (req, res) => {
 
+    try {
+        await productModel.findByIdAndDelete(req.body.id);
+        res.json({ success: true, message: "Product removed!" });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
+
 }
 
 const singleProductInfo = async (req, res) => {
+
+    try {
+        const { productId } = req.body;
+        const product = await productModel.findById(productId);
+        res.json({ success: true, product });
+    } catch (error) {
+        console.log(error);
+        res.json({ success: false, message: error.message });
+    }
 
 }
 
