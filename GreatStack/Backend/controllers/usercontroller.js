6a7710d7fe6a4281373fdc24e0ab0e122c631438
Kaushlendra <kaushlendrapratapsingh631@gmail.com>
2025-11-29 21:23:24 +0530

    Just fixed format.

--- GreatStack/Backend/controllers/usercontroller.js
+++ GreatStack/Backend/controllers/usercontroller.js
@@ -69,17 +69,17 @@ const registerUser = async (req, res) => {
 //route for admiin login
 const loginAdmin = async (req, res) => {
     try {
-        const {email,password} = req.body;
-        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
-            const token = jwt.sign(email+password,process.env.JWT_SECRET);
-            res.json({success:true,token});
+        const { email, password } = req.body;
+        if (email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD) {
+            const token = jwt.sign(email + password, process.env.JWT_SECRET);
+            res.json({ success: true, token });
         }
-        else{
-            res.json({success:false,message:"Invalid Credentials!"});
+        else {
+            res.json({ success: false, message: "Invalid Credentials!" });
         }
     } catch (error) {
         console.log(error);
-        res.json({success:false,message:error.message});
+        res.json({ success: false, message: error.message });
     }
 }
 
